7d585ab2364075b0f4187915992b9d3fd94a56bf
drhu <lbkrpaw87@gmail.com>
2019-02-22 10:14:33 +0100

    number fixin

--- src/app/fib-calc/fib-calc.component.ts
+++ src/app/fib-calc/fib-calc.component.ts
@@ -12,27 +12,27 @@ export class FibCalc implements OnInit {
 
  public Range: number
  public type: String
- public result_236: number;
- public result_382: number;
- public result_50: number;
- public result_618: number;
+ public result_236: string;
+ public result_382: string;
+ public result_50: string;
+ public result_618: string;
 
 
 cal_fib(){
 
   if ( this.type === 'Long'){
     this.Range = this.High - this.Low
-    this.result_236 = (this.Range * 0.764) + this.Low
-    this.result_382 =(this.Range * 0.618)+this.Low
-    this.result_50 =(this.Range * 0.50)+this.Low
-    this.result_618 =(this.Range * 0.382)+this.Low
+    this.result_236 = ((this.Range * 0.764) + this.Low).toFixed(5)
+    this.result_382 =((this.Range * 0.618)+this.Low).toFixed(5)
+    this.result_50 =((this.Range * 0.50)+this.Low).toFixed(5)
+    this.result_618 =((this.Range * 0.382)+this.Low).toFixed(5)
   }
   if (this.type === 'Short'){
     this.Range = this.High - this.Low
-    this.result_236 = this.High - (this.Range * 0.764)
-    this.result_382 = this.High - (this.Range * 0.618)
-    this.result_50 = this.High - (this.Range * 0.50)
-    this.result_618 = this.High - (this.Range * 0.382)
+    this.result_236 = (this.High - (this.Range * 0.764)).toFixed(5)
+    this.result_382 = (this.High - (this.Range * 0.618)).toFixed(5)
+    this.result_50 = (this.High - (this.Range * 0.50)).toFixed(5)
+    this.result_618 = (this.High - (this.Range * 0.382)).toFixed(5)
 
   }
   };
