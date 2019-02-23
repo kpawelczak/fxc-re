b3a1053da464c22cf78991cc7a8b636fe5547b0f
drhu <lbkrpaw87@gmail.com>
2019-02-23 21:27:52 +0100

    input form migrate

--- src/app/fib-calc/fib-calc.component.ts
+++ src/app/fib-calc/fib-calc.component.ts
@@ -9,32 +9,26 @@ import { InputValues } from '../input-values';
 export class FibCalcComponent {
 
     ivalue = InputValues;
-            
-    public Range: number
-    public type: String 
 
-    public result_236: string
-    public result_382: string
-    public result_50: string
-    public result_618: string
+    public Range: number
 
     cal_fib() {
 
         this.Range = this.ivalue.High - this.ivalue.Low
 
-        if (this.type === 'Long') {
+        if (this.ivalue.type === 'Long') {
 
-            this.result_236 = ((this.Range * 0.764) + this.ivalue.Low).toFixed(5)
-            this.result_382 = ((this.Range * 0.618) + this.ivalue.Low).toFixed(5)
-            this.result_50 = ((this.Range * 0.50) + this.ivalue.Low).toFixed(5)
-            this.result_618 = ((this.Range * 0.382) + this.ivalue.Low).toFixed(5)
+            this.ivalue.result_236 = ((this.Range * 0.764) + this.ivalue.Low).toFixed(5)
+            this.ivalue.result_382 = ((this.Range * 0.618) + this.ivalue.Low).toFixed(5)
+            this.ivalue.result_50 = ((this.Range * 0.50) + this.ivalue.Low).toFixed(5)
+            this.ivalue.result_618 = ((this.Range * 0.382) + this.ivalue.Low).toFixed(5)
         }
-        if (this.type === 'Short') {
+        if (this.ivalue.type === 'Short') {
 
-            this.result_236 = (this.ivalue.High - (this.Range * 0.764)).toFixed(5)
-            this.result_382 = (this.ivalue.High - (this.Range * 0.618)).toFixed(5)
-            this.result_50 = (this.ivalue.High - (this.Range * 0.50)).toFixed(5)
-            this.result_618 = (this.ivalue.High - (this.Range * 0.382)).toFixed(5)
+            this.ivalue.result_236 = (this.ivalue.High - (this.Range * 0.764)).toFixed(5)
+            this.ivalue.result_382 = (this.ivalue.High - (this.Range * 0.618)).toFixed(5)
+            this.ivalue.result_50 = (this.ivalue.High - (this.Range * 0.50)).toFixed(5)
+            this.ivalue.result_618 = (this.ivalue.High - (this.Range * 0.382)).toFixed(5)
 
         }
     };
