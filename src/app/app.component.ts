e7fe06871e0380cb0cc791e7eef9dc32e659a11a
drhu <lbkrpaw87@gmail.com>
2019-05-18 10:21:49 +0200

    #4 Reduce Input and Outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,18 +21,8 @@ export class AppComponent {
 	public Open;
 	public Close;
 
-	public result_236;
-	public result_382;
-	public result_50;
-	public result_618;
-
-	public result_R3;
-	public result_R2;
-	public result_R1;
-	public result_PP;
-	public result_S1;
-	public result_S2;
-	public result_S3;
+	public fiboResults: Array<string>;
+	public pivotResults: Array<string>;
 
 	fiboChecked(data) {
 		this.showFibo = data;
@@ -53,12 +43,8 @@ export class AppComponent {
 	techBtnChecked() {
 
 		if (!this.showTechnicals) {
-			if (this.showFibo) {
-				this.technicalsCheckedFibo = true;
-			} else this.technicalsCheckedFibo = false;
-			if (this.showPivot) {
-				this.technicalsCheckedPivot = true;
-			} else this.technicalsCheckedPivot = false;
+			this.technicalsCheckedFibo = this.showFibo;
+			this.technicalsCheckedPivot = this.showPivot;
 		}
 	}
 
@@ -86,48 +72,11 @@ export class AppComponent {
 
 	// Calculations Data
 
-	set236FromCalc(Data2) {
-		this.result_236 = Data2;
+	appCalculatorFiboResults(data) {
+		this.fiboResults = data;
 	}
 
-	set382FromCalc(Data3) {
-		this.result_382 = Data3;
+	appCalculatorPivotResults(data) {
+		this.pivotResults = data;
 	}
-
-	set50FromCalc(Data5) {
-		this.result_50 = Data5;
-	}
-
-	set618FromCalc(Data6) {
-		this.result_618 = Data6;
-	}
-
-	setR3FromCalc(DataR3) {
-		this.result_R3 = DataR3;
-	}
-
-	setR2FromCalc(DataR2) {
-		this.result_R2 = DataR2;
-	}
-
-	setR1FromCalc(DataR1) {
-		this.result_R1 = DataR1;
-	}
-
-	setPPFromCalc(DataPP) {
-		this.result_PP = DataPP;
-	}
-
-	setS1FromCalc(DataS1) {
-		this.result_S1 = DataS1;
-	}
-
-	setS2FromCalc(DataS2) {
-		this.result_S2 = DataS2;
-	}
-
-	setS3FromCalc(DataS3) {
-		this.result_S3 = DataS3;
-	}
-
 }
