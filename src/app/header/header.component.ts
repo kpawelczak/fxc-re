8015157200fd90843d78a731cd7eb84e8df63a0b
drhu <lbkrpaw87@gmail.com>
2019-10-07 14:56:12 +0200

    small change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,32 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
 	positionsActive: boolean = false;
+
+	private routerSubscription: Subscription;
 
 	constructor(private router: Router) {
 	}
 
 	ngOnInit(): void {
-		this.router
-			.events
-			.pipe(
-				filter((event: any) => event instanceof NavigationEnd)
-			)
-			.subscribe(event => {
-				if (event.url === '/' || event.url === '/positions') {
-					this.positionsActive = true;
-				} else {
-					this.positionsActive = false;
-				}
-			});
+		this.routerSubscription =
+			this.router.events
+				.pipe(
+					filter((event: any) => event instanceof NavigationEnd)
+				)
+				.subscribe(event => {
+					if (event.url === '/' || event.url === '/positions') {
+						this.positionsActive = true;
+					} else {
+						this.positionsActive = false;
+					}
+				});
+	}
+
+	ngOnDestroy() {
+		this.routerSubscription.unsubscribe();
 	}
 
 }
